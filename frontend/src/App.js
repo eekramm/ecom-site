fc3422dbf885ae7d15b8a96dec46e70219357fcd
Eythan Kramm <eythankramm@Eythans-MBP.attlocal.net>
2021-12-14 22:42:20 -0500

    added the HomeScreen and created the Product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
+import "./bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to Eythan's Shop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
